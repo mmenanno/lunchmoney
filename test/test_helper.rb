caa60904533b53ef03e88a15ec8eea2bcedd5f5b
halorrr <mvmenanno@gmail.com>
2024-01-26 16:43:28 -0500

    Configure VCR and webmock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,9 +9,45 @@ require "minitest/autorun"
 require "minitest/pride"
 require "active_support"
 require "mocha/minitest"
+require "webmock/minitest"
+require "vcr"
 require "pry"
 
 require_relative "helpers/configuration_stubs"
 require_relative "helpers/mocha_typed"
 require_relative "helpers/mock_response_helper"
 require_relative "helpers/fake_response_data_helper"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/cassettes"
+  config.hook_into(:webmock)
+  config.default_cassette_options = { record: :once }
+
+  config.filter_sensitive_data("<BEARER_TOKEN>") do |interaction|
+    auths = interaction.request.headers["Authorization"].first
+    if (match = auths.match(/^Bearer\s+([^,\s]+)/))
+      match.captures.first
+    end
+  end
+end
+
+if ENV.fetch("REMOTE_TESTS_ENABLED", nil)
+  puts "Remote tests are enabled, ignoring VCR cassettes and enabling real HTTP connections"
+  VCR.turn_off!(ignore_cassettes: true)
+  WebMock.enable_net_connect!
+else
+  puts "Remote tests are disabled, using VCR cassettes and disabling real HTTP connections"
+  VCR.turn_on!
+  WebMock.disable_net_connect!
+end
+
+module ActiveSupport
+  class TestCase
+    sig { void }
+    def ensure_correct_api_key
+      LunchMoney.configure do |config|
+        config.api_key = ENV.fetch("LUNCHMONEY_TOKEN", nil)
+      end
+    end
+  end
+end
