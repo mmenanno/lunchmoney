f99b1563a845883b859ebf47714d964b6d897e29
halorrr <mvmenanno@gmail.com>
2024-01-26 02:44:14 -0500

    finish plaid_accounts call

--- lib/lunchmoney/plaid_accounts/plaid_account.rb
+++ lib/lunchmoney/plaid_accounts/plaid_account.rb
@@ -17,10 +17,12 @@ module LunchMoney
       :last_import,
       :balance,
       :currency,
-      :balance_last_update
+      :balance_last_update,
+      :display_name,
+      :plaid_last_successful_update
 
     sig { returns(T.nilable(String)) }
-    attr_accessor :subtype
+    attr_accessor :subtype, :import_start_date, :last_fetch
 
     sig { returns(T.nilable(Integer)) }
     attr_accessor :limit
@@ -37,16 +39,22 @@ module LunchMoney
         balance: String,
         currency: String,
         balance_last_update: String,
+        display_name: String,
         id: Integer,
+        plaid_last_successful_update: String,
         limit: T.nilable(Integer),
         subtype: T.nilable(String),
+        import_start_date: T.nilable(String),
+        last_fetch: T.nilable(String),
       ).void
     end
-    def initialize(date_linked:, name:, type:, mask:, institution_name:, status:, last_import:,
-      balance:, currency:, balance_last_update:, id:, limit: nil, subtype: nil)
+    def initialize(date_linked:, name:, type:, mask:, institution_name:, status:, last_import:, balance:, currency:,
+      balance_last_update:, display_name:, id:, plaid_last_successful_update:, limit: nil, subtype: nil,
+      import_start_date: nil, last_fetch: nil)
       super()
       @date_linked = date_linked
       @name = name
+      @display_name = display_name
       @type = type
       @mask = mask
       @institution_name = institution_name
@@ -56,8 +64,12 @@ module LunchMoney
       @currency = currency
       @balance_last_update = balance_last_update
       @id = id
+      @plaid_last_successful_update = plaid_last_successful_update
       @limit = limit
       @subtype = subtype
+      @balance_last_update = balance_last_update
+      @import_start_date = import_start_date
+      @last_fetch = last_fetch
     end
   end
 end
---
+++ test/lunchmoney/plaid_accounts/plaid_account_calls_test.rb
@@ -0,0 +1,29 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "test_helper"
+
+class PlaidAccountCallsTest < ActiveSupport::TestCase
+  include MockResponseHelper
+  include FakeResponseDataHelper
+
+  test "plaid_accounts returns an array of PlaidAccount objects on success response" do
+    response = mock_faraday_response(fake_plaid_accounts_call)
+
+    LunchMoney::PlaidAccountCalls.any_instance.stubs(:get).returns(response)
+
+    api_call = LunchMoney::PlaidAccountCalls.new.plaid_accounts
+
+    assert_kind_of(LunchMoney::PlaidAccount, api_call.first)
+  end
+
+  test "plaid_accounts returns an array of Error objects on error response" do
+    response = mock_faraday_response(fake_general_error)
+
+    LunchMoney::PlaidAccountCalls.any_instance.stubs(:get).returns(response)
+
+    api_call = LunchMoney::PlaidAccountCalls.new.plaid_accounts
+
+    assert_kind_of(LunchMoney::Error, api_call.first)
+  end
+end
