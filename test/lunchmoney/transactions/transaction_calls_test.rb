52451a708c462a799cc012b062761178b1c7bed6
halorrr <mvmenanno@gmail.com>
2024-01-28 23:13:07 -0500

    finish insert transaction call

--- test/lunchmoney/transactions/transaction_calls_test.rb
+++ test/lunchmoney/transactions/transaction_calls_test.rb
@@ -64,4 +64,39 @@ class TransactionCallsTest < ActiveSupport::TestCase
       assert_kind_of(LunchMoney::Error, error)
     end
   end
+
+  test "insert_transactions returns a hash containing an array of ids on success response" do
+    VCR.use_cassette("transactions/insert_transactions_success") do
+      api_call = LunchMoney::TransactionCalls.new.insert_transactions([random_update_transaction])
+      ids = T.cast(api_call, T::Hash[Symbol, T::Array[Integer]])[:ids]
+
+      refute_nil(ids)
+
+      T.unsafe(ids).each do |id|
+        assert_kind_of(Integer, id)
+      end
+    end
+  end
+
+  test "insert_transactions returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::TransactionCalls.any_instance.stubs(:post).returns(response)
+
+    api_call = LunchMoney::TransactionCalls.new.insert_transactions([random_update_transaction])
+
+    T.unsafe(api_call).each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
+
+  private
+
+  sig { returns(LunchMoney::UpdateTransaction) }
+  def random_update_transaction
+    date = Time.now.utc.strftime("%F")
+    amount = rand(0.1..99.9).to_s
+    payee = "Gem Remote Testing"
+    notes = "Remote test at #{Time.now.utc}"
+    LunchMoney::UpdateTransaction.new(date:, amount:, payee:, notes:)
+  end
 end
