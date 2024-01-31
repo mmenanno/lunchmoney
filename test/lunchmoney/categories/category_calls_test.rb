305d8fbffa08ae1a81fa14e34b7967dd3e9b6310
halorrr <mvmenanno@gmail.com>
2024-01-30 19:55:47 -0500

    finish remaining category endpoints

--- test/lunchmoney/categories/category_calls_test.rb
+++ test/lunchmoney/categories/category_calls_test.rb
@@ -95,4 +95,120 @@ class CategoryCallsTest < ActiveSupport::TestCase
       end
     end
   end
+
+  test "create_category returns an id of created category success response" do
+    VCR.use_cassette("categories/create_category_success") do
+      api_call = LunchMoney::CategoryCalls.new.create_category(name: "Create Category Test")
+      api_call = T.cast(api_call, T::Hash[Symbol, Integer])
+
+      assert_kind_of(Integer, api_call[:category_id])
+    end
+  end
+
+  test "create_category returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::CategoryCalls.any_instance.stubs(:post).returns(response)
+
+    api_call = LunchMoney::CategoryCalls.new.create_category(name: "Create Category Test")
+
+    api_call.each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
+
+  test "create_category_group returns anid of created category group on success response" do
+    VCR.use_cassette("categories/create_category_group_success") do
+      api_call = LunchMoney::CategoryCalls.new.create_category_group(name: "Create Category Group Test")
+      api_call = T.cast(api_call, T::Hash[Symbol, Integer])
+
+      assert_kind_of(Integer, api_call[:category_id])
+    end
+  end
+
+  test "create_category_group returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::CategoryCalls.any_instance.stubs(:post).returns(response)
+
+    api_call = LunchMoney::CategoryCalls.new.create_category_group(name: "Create Category Group Test")
+
+    api_call.each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
+
+  test "update_category returns a boolean on success response" do
+    VCR.use_cassette("categories/update_category_success") do
+      api_call = LunchMoney::CategoryCalls.new.update_category(784587, name: "Update Category Test")
+
+      assert_includes([TrueClass, FalseClass], api_call.class)
+    end
+  end
+
+  test "update_category returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::CategoryCalls.any_instance.stubs(:put).returns(response)
+
+    api_call = LunchMoney::CategoryCalls.new.update_category(784587, name: "Update Category Test")
+
+    T.unsafe(api_call).each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
+
+  test "add_to_category_group returns a Category object on success response" do
+    VCR.use_cassette("categories/add_to_category_group_success") do
+      api_call = LunchMoney::CategoryCalls.new.add_to_category_group(784588, new_categories: ["New Category Test"])
+
+      assert_kind_of(LunchMoney::Category, api_call)
+    end
+  end
+
+  test "add_to_category_group returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::CategoryCalls.any_instance.stubs(:post).returns(response)
+
+    api_call = LunchMoney::CategoryCalls.new.add_to_category_group(784588, new_categories: ["New Category Test"])
+
+    T.unsafe(api_call).each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
+
+  test "delete_category returns a boolean on success response" do
+    VCR.use_cassette("categories/delete_category_success") do
+      api_call = LunchMoney::CategoryCalls.new.delete_category(784587)
+
+      assert_includes([TrueClass, FalseClass], api_call.class)
+    end
+  end
+
+  test "delete_category returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::CategoryCalls.any_instance.stubs(:delete).returns(response)
+
+    api_call = LunchMoney::CategoryCalls.new.delete_category(784587)
+
+    T.unsafe(api_call).each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
+
+  test "force_delete_category returns a boolean on success response" do
+    VCR.use_cassette("categories/force_delete_category_success") do
+      api_call = LunchMoney::CategoryCalls.new.force_delete_category(784588)
+
+      assert_includes([TrueClass, FalseClass], api_call.class)
+    end
+  end
+
+  test "force_delete_category returns an array of Error objects on error response" do
+    response = mock_faraday_lunchmoney_error_response
+    LunchMoney::CategoryCalls.any_instance.stubs(:delete).returns(response)
+
+    api_call = LunchMoney::CategoryCalls.new.force_delete_category(784588)
+
+    T.unsafe(api_call).each do |error|
+      assert_kind_of(LunchMoney::Error, error)
+    end
+  end
 end
