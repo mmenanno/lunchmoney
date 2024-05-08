c7199e5de0faea8d4e134af5fc8485260d79d03d
Saalik Lokhandwala <saalikl111@gmail.com>
2024-05-08 14:08:45 +0800

    add primary_currency to user object

--- lib/lunchmoney/objects/user.rb
+++ lib/lunchmoney/objects/user.rb
@@ -21,16 +21,18 @@ module LunchMoney
           user_email: String,
           account_id: Integer,
           budget_name: String,
+          primary_currency: String,
           api_key_label: T.nilable(String),
         ).void
       end
-      def initialize(user_id:, user_name:, user_email:, account_id:, budget_name:, api_key_label: nil)
+      def initialize(user_id:, user_name:, user_email:, account_id:, budget_name:, primary_currency:, api_key_label: nil)
         super()
         @user_id = user_id
         @user_name = user_name
         @user_email = user_email
         @account_id = account_id
         @budget_name = budget_name
+        @primary_currency = primary_currency
         @api_key_label = api_key_label
       end
     end
