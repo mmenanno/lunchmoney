cbb6f83f01ff0451bd2d6272c6303b4f75adc458
mmenanno <1358708+mmenanno@users.noreply.github.com>
2026-02-16 17:13:18 -0500

    feat: create v2 error hierarchy (0101)

--- test/lunchmoney/errors_test.rb
+++ test/lunchmoney/errors_test.rb
@@ -1,44 +1,138 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require "minitest/autorun"
+require_relative "../../lib/lunchmoney/errors"
 
-module LunchMoney
-  class ErrorsTest < ActiveSupport::TestCase
-    setup do
-      @error = LunchMoney::Errors.new
+class LunchMoneyErrorsTest < Minitest::Test
+  def test_error_inherits_from_standard_error
+    assert LunchMoney::Error < StandardError
+  end
+
+  def test_api_error_inherits_from_error
+    assert LunchMoney::ApiError < LunchMoney::Error
+  end
+
+  def test_authentication_error_inherits_from_api_error
+    assert LunchMoney::AuthenticationError < LunchMoney::ApiError
+  end
+
+  def test_not_found_error_inherits_from_api_error
+    assert LunchMoney::NotFoundError < LunchMoney::ApiError
+  end
+
+  def test_validation_error_inherits_from_api_error
+    assert LunchMoney::ValidationError < LunchMoney::ApiError
+  end
+
+  def test_rate_limit_error_inherits_from_api_error
+    assert LunchMoney::RateLimitError < LunchMoney::ApiError
+  end
+
+  def test_server_error_inherits_from_api_error
+    assert LunchMoney::ServerError < LunchMoney::ApiError
+  end
+
+  def test_configuration_error_inherits_from_error
+    assert LunchMoney::ConfigurationError < LunchMoney::Error
+  end
+
+  def test_invalid_api_key_inherits_from_configuration_error
+    assert LunchMoney::InvalidApiKey < LunchMoney::ConfigurationError
+  end
+
+  def test_api_error_attributes
+    error = LunchMoney::ApiError.new(
+      status_code: 400,
+      message: "Bad request",
+      errors: ["field is required"],
+      response: "raw_response",
+      rate_limit: "rate_limit_obj"
+    )
+
+    assert_equal 400, error.status_code
+    assert_equal "Bad request", error.message
+    assert_equal ["field is required"], error.errors
+    assert_equal "raw_response", error.response
+    assert_equal "rate_limit_obj", error.rate_limit
+  end
+
+  def test_api_error_default_values
+    error = LunchMoney::ApiError.new(status_code: 500, message: "Server error")
+
+    assert_equal [], error.errors
+    assert_nil error.response
+    assert_nil error.rate_limit
+  end
+
+  def test_rate_limit_error_retry_after
+    error = LunchMoney::RateLimitError.new(
+      retry_after: 30,
+      status_code: 429,
+      message: "Rate limited"
+    )
+
+    assert_equal 30, error.retry_after
+    assert_equal 429, error.status_code
+    assert_equal "Rate limited", error.message
+  end
+
+  def test_rate_limit_error_retry_after_defaults_to_nil
+    error = LunchMoney::RateLimitError.new(status_code: 429, message: "Rate limited")
+
+    assert_nil error.retry_after
+  end
+
+  def test_rescue_api_error_catches_http_subclasses
+    [
+      LunchMoney::AuthenticationError,
+      LunchMoney::NotFoundError,
+      LunchMoney::ValidationError,
+      LunchMoney::RateLimitError,
+      LunchMoney::ServerError,
+    ].each do |klass|
+      assert_raises(LunchMoney::ApiError) do
+        raise klass.new(status_code: 500, message: "test")
+      end
+    end
+  end
+
+  def test_rescue_error_catches_all_subclasses
+    assert_raises(LunchMoney::Error) do
+      raise LunchMoney::ApiError.new(status_code: 500, message: "test")
     end
 
-    test "initializes error with message" do
-      error = LunchMoney::Errors.new(message: "Some error message")
-
-      assert_equal(["Some error message"], error.messages)
+    assert_raises(LunchMoney::Error) do
+      raise LunchMoney::ConfigurationError, "test"
     end
 
-    test "initializes error without message" do
-      error = LunchMoney::Errors.new
-
-      assert_empty(error.messages)
+    assert_raises(LunchMoney::Error) do
+      raise LunchMoney::InvalidApiKey, "test"
     end
+  end
 
-    test "can add messages to error" do
-      @error.messages << "Error 1"
-      @error.messages << "Error 2"
+  def test_each_error_class_can_be_instantiated
+    api_error = LunchMoney::ApiError.new(status_code: 400, message: "test")
+    assert_instance_of LunchMoney::ApiError, api_error
 
-      assert_equal(["Error 1", "Error 2"], @error.messages)
-    end
+    auth_error = LunchMoney::AuthenticationError.new(status_code: 401, message: "test")
+    assert_instance_of LunchMoney::AuthenticationError, auth_error
 
-    test "can get first message" do
-      @error.messages << "Error 1"
-      @error.messages << "Error 2"
+    not_found = LunchMoney::NotFoundError.new(status_code: 404, message: "test")
+    assert_instance_of LunchMoney::NotFoundError, not_found
 
-      assert_equal("Error 1", @error.first)
-    end
+    validation = LunchMoney::ValidationError.new(status_code: 422, message: "test")
+    assert_instance_of LunchMoney::ValidationError, validation
 
-    test "can check if error messages are empty" do
-      assert_empty(@error)
-      @error.messages << "Error"
+    rate_limit = LunchMoney::RateLimitError.new(status_code: 429, message: "test")
+    assert_instance_of LunchMoney::RateLimitError, rate_limit
 
-      refute_empty(@error)
-    end
+    server = LunchMoney::ServerError.new(status_code: 500, message: "test")
+    assert_instance_of LunchMoney::ServerError, server
+
+    config = LunchMoney::ConfigurationError.new("test")
+    assert_instance_of LunchMoney::ConfigurationError, config
+
+    invalid_key = LunchMoney::InvalidApiKey.new("test")
+    assert_instance_of LunchMoney::InvalidApiKey, invalid_key
   end
 end
