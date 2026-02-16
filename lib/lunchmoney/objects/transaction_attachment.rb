255ff4f87eb6388236330537a506d1b1b1ffb25e
mmenanno <1358708+mmenanno@users.noreply.github.com>
2026-02-16 17:24:11 -0500

    feat: generate transaction attachment model (0210)

---
+++ lib/lunchmoney/objects/transaction_attachment.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# AUTO-GENERATED from LunchMoney OpenAPI spec v2.8.5
+# Do not edit manually. Run `toys generate models` to regenerate.
+
+module LunchMoney
+  module Objects
+    class TransactionAttachment < Base
+      attr_accessor :id, :uploaded_by, :name, :type, :size, :notes, :created_at
+    end
+  end
+end
